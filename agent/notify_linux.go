84ae2f35c793f6fa602a760255b9e0e3936133ef
Kevin King <4kevinking@gmail.com>
2016-09-10 17:53:15 -0400

    notifylinux impl

--- agent/notify_linux.go
+++ agent/notify_linux.go
@@ -1,6 +1,14 @@
 package main
 
+import (
+	"github.com/0xAX/notificator"
+)
+
 func DesktopNotify(message string) (err error) {
-	//TODO
+	notify = notificator.New(notificator.Options{
+		AppName: "krssh-agent",
+	})
+
+	notify.Push("krssh-agent", message, "", notificator.UR_CRITICAL)
 	return
 }
