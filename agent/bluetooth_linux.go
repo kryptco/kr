5af274bb9ad71875e46dacc2f69e87420c179d3d
Kevin King <4kevinking@gmail.com>
2016-09-19 10:50:28 -0400

    add linux bluetooth stub

---
+++ agent/bluetooth_linux.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bitbucket.org/kryptco/go.corebluetooth"
+	"github.com/satori/go.uuid"
+)
+
+type BluetoothDriver struct {
+}
+
+func NewBluetoothDriver() (bt *BluetoothDriver, err error) {
+	bt = &BluetoothDriver{}
+	return
+}
+
+func (bt *BluetoothDriver) AddService(serviceUUID uuid.UUID) (err error) {
+	return
+}
+func (bt *BluetoothDriver) RemoveService(serviceUUID uuid.UUID) (err error) {
+	return
+}
+func (bt *BluetoothDriver) ReadChan() (readChan chan []byte, err error) {
+	readChan = make(chan []byte)
+	close(readChan)
+	return
+}
+func (bt *BluetoothDriver) Write(data []byte) (err error) {
+	return
+}
