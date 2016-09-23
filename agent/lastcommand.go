505883437488d13e3e6eab29c54a75b16bcc3c5e
Kevin King <4kevinking@gmail.com>
2016-09-22 21:44:17 -0400

    take last line for bash_history

--- agent/lastcommand.go
+++ agent/lastcommand.go
@@ -47,11 +47,11 @@ func getLastBashCommand() *command {
 	}
 	histStr := string(hist)
 	lines := strings.Split(histStr, "\n")
-	if len(lines) < 2 {
+	if len(lines) == 0 {
 		return nil
 	}
 
-	lastCommand := lines[len(lines)-2]
+	lastCommand := lines[len(lines)-1]
 	stat, err := os.Stat(file)
 	if err != nil {
 		return nil
