44f6fe3127eaed0c92765e59d0d7b40b2a84ce55
Kevin King <4kevinking@gmail.com>
2016-11-09 22:30:18 -0500

    add rejected ✘

--- krdclient/krdclient.go
+++ krdclient/krdclient.go
@@ -12,7 +12,7 @@ import (
 var ErrNotPaired = fmt.Errorf("Workstation not yet paired. Please run \"kr pair\" and scan the QRCode with the Kryptonite mobile app.")
 var ErrTimedOut = fmt.Errorf("Request timed out. Make sure your phone and workstation are paired and connected to the internet and the Kryptonite app is running.")
 var ErrSigning = fmt.Errorf("Kryptonite was unable to perform SSH login. Please restart the Kryptonite app on your phone.")
-var ErrRejected = fmt.Errorf("Rejected by Kryptonite.")
+var ErrRejected = fmt.Errorf("Request Rejected ✘")
 
 func RequestMe() (me kr.Profile, err error) {
 	meRequest, err := kr.NewRequest()
