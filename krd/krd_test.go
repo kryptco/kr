4cf41f18cca149be83b528e34229072ad5995313
Kevin King <4kevinking@gmail.com>
2016-11-28 15:48:44 -0500

    add krd_test to ensure GOMAXPROCS > 1

---
+++ krd/krd_test.go
@@ -0,0 +1,16 @@
+package main
+
+import(
+	"testing"
+	"runtime"
+	"flag"
+	"os"
+)
+
+func TestMain(m *testing.M) {
+	if runtime.GOMAXPROCS(0) == 1 {
+		runtime.GOMAXPROCS(4)
+	}
+	flag.Parse()
+	os.Exit(m.Run())
+}
