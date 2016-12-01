0725d1ddf0fa02f227ac8999430842a01c9f6383
Kevin King <4kevinking@gmail.com>
2016-12-01 14:41:21 -0500

    bump version

--- version_linux.go
+++ version_linux.go
@@ -4,7 +4,7 @@ import (
 	"github.com/blang/semver"
 )
 
-var CURRENT_VERSION = semver.MustParse("1.0.13")
+var CURRENT_VERSION = semver.MustParse("1.0.14")
 
 func GetLatestVersion() (version semver.Version, err error) {
 	versions, err := GetLatestVersions()
