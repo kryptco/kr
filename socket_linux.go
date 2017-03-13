7280a1b218c7ea8e64345e80a413b980b14f955f
Kevin King <4kevinking@gmail.com>
2017-03-13 13:43:37 -0400

    fix fallback linux socket logic

--- socket_linux.go
+++ socket_linux.go
@@ -11,15 +11,15 @@ func DaemonDial(unixFile string) (conn net.Conn, err error) {
 	conn, err = net.Dial("unix", unixFile)
 	if err != nil {
 		//	restart then try again
+		exec.Command("systemctl", "--user", "disable", "kr").Run()
 		exec.Command("systemctl", "--user", "enable", "kr").Run()
 		exec.Command("systemctl", "--user", "stop", "kr").Run()
 		exec.Command("systemctl", "--user", "start", "kr").Run()
 		<-time.After(time.Second)
-		if err == nil {
-			conn, err = net.Dial("unix", unixFile)
-		} else {
-			err = fmt.Errorf("Failed to connect to Kryptonite daemon. Please make sure it is running by typing \"kr restart\".")
-		}
+		conn, err = net.Dial("unix", unixFile)
+	}
+	if err != nil {
+		err = fmt.Errorf("Failed to connect to Kryptonite daemon. Please make sure it is running by typing \"kr restart\".")
 	}
 	return
 }
