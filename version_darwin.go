02c98823aaf581d7e6a91ee7f35f54c336ecb550
Kevin King <kevin@krypt.co>
2017-06-03 19:17:05 -0400

    bump version to 2.1.2

--- version_darwin.go
+++ version_darwin.go
@@ -4,7 +4,7 @@ import (
 	"github.com/blang/semver"
 )
 
-var CURRENT_VERSION = semver.MustParse("2.1.1")
+var CURRENT_VERSION = semver.MustParse("2.1.2")
 
 func GetLatestVersion() (version semver.Version, err error) {
 	versions, err := GetLatestVersions()
