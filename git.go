c9fe36914e2497ef34928ac66f8b232911d3353c
Kevin King <4kevinking@gmail.com>
2017-07-14 18:18:00 -0400

    make sure /usr/bin and /usr/local/bin are on krd's path when running git config

--- git.go
+++ git.go
@@ -2,11 +2,16 @@ package kr
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 	"strings"
 )
 
 func GlobalGitUserId() (id string, err error) {
+	oldPath := os.Getenv("PATH")
+	if !strings.Contains(oldPath, "/usr/local/bin") || !strings.Contains(oldPath, "/usr/bin") {
+		os.Setenv("PATH", "/usr/bin:/usr/local/bin:"+oldPath)
+	}
 	name, err := exec.Command("git", "config", "--global", "user.name").Output()
 	if err != nil {
 		return
