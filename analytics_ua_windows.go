137959f353b196bcbb651ece0486567d53714bfe
Matt Peterson <matt@implbits.com>
2017-07-24 21:04:26 -0600

    Report OS correctly

--- analytics_ua_windows.go
+++ analytics_ua_windows.go
@@ -10,7 +10,7 @@ import (
 
 var analytics_user_agent = fmt.Sprintf("Mozilla/5.0 (10.0.15063; Win64) (KHTML, like Gecko) Version/%s kr/%s", CURRENT_VERSION, CURRENT_VERSION)
 
-const analytics_os = "Linux"
+const analytics_os = "Windows"
 
 var cachedAnalyticsOSVersion *string
 var osVersionMutex sync.Mutex
