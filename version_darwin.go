114157f9788ba48f34bfac0662e83771e2b27d5f
Kevin King <4kevinking@gmail.com>
2017-08-08 17:22:47 -0400

    crank version

--- version_darwin.go
+++ version_darwin.go
@@ -4,7 +4,7 @@ import (
 	"github.com/blang/semver"
 )
 
-var CURRENT_VERSION = semver.MustParse("2.2.5")
+var CURRENT_VERSION = semver.MustParse("2.2.6")
 
 func GetLatestVersion() (version semver.Version, err error) {
 	versions, err := GetLatestVersions()
