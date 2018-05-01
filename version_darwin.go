7707212b25b2ffcd6698e76d07c61f54d78f24e1
Kevin King <4kevinking@gmail.com>
2018-04-30 23:33:28 -0400

    Bump version to 2.4.4

--- version_darwin.go
+++ version_darwin.go
@@ -4,7 +4,7 @@ import (
 	"github.com/blang/semver"
 )
 
-var CURRENT_VERSION = semver.MustParse("2.4.3")
+var CURRENT_VERSION = semver.MustParse("2.4.4")
 
 func GetLatestVersion() (version semver.Version, err error) {
 	versions, err := GetLatestVersions()
--- version_unix.go
+++ version_unix.go
@@ -6,7 +6,7 @@ import (
 	"github.com/blang/semver"
 )
 
-var CURRENT_VERSION = semver.MustParse("2.4.3")
+var CURRENT_VERSION = semver.MustParse("2.4.4")
 
 func GetLatestVersion() (version semver.Version, err error) {
 	versions, err := GetLatestVersions()
