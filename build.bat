d8e381f32b8815c95ebf3a59095df6a7fe592e86
Damjan Cvetko <damjan.cvetko@monotek.net>
2019-10-23 00:46:06 +0200

    trimpath

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 mkdir bin
 cd src
-go.exe build -v -o ../bin/kr.exe ./kr
-go.exe build -v -o ../bin/krd.exe ./krd
-go.exe build -v -o ../bin/krssh.exe ./krssh
+go.exe build -v -trimpath -o ../bin/kr.exe ./kr
+go.exe build -v -trimpath -o ../bin/krd.exe ./krd
+go.exe build -v -trimpath -o ../bin/krssh.exe ./krssh
 cd ..
